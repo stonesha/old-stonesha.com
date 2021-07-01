0b8fd9428c7216457b0072bd49d07006f07b10f3
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-06-30 18:50:49 -0700

    removed (WIP)

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <Container>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          Hey, I’m Stone Sha (WIP)
+          Hey, I’m Stone Sha
         </h1>
         <h2 className="prose text-gray-600 dark:text-gray-400 mb-16">
           I'm a developer, guitar player, and video game enthusiast. I use this website personally to
