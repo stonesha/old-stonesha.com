36b47e577d770c5885d37ba5ee7e0792a876e4c0
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-07-13 15:23:28 -0700

    converted class to className

--- components/MangaCard.js
+++ components/MangaCard.js
@@ -12,25 +12,25 @@ export default function MangaCard({ tag }) {
         return null;
 
     return (
-        <div class="max-w-sm w-full lg:max-w-full lg:flex">
-            <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+        <div className="max-w-sm w-full lg:max-w-full lg:flex">
+            <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
                 <img src={manga.img_url} alt={manga.title} />
             </div>
-            <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 dark:border-gray-900
+            <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 dark:border-gray-900
              rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                    <div class="text-gray-900 dark:text-gray-100 font-bold text-xl mb-2">{manga.title}</div>
-                    <p class="text-gray-700 dark:text-gray-400 text-base">{parse(manga.description)}</p>
+                <div className="mb-8">
+                    <div className="text-gray-900 dark:text-gray-100 font-bold text-xl mb-2">{manga.title}</div>
+                    <p className="text-gray-700 dark:text-gray-400 text-base">{parse(manga.description)}</p>
                 </div>
-                <div class="flex items-center">
-                    <div class="text-sm">
-                        <p class="text-gray-700 dark:text-gray-400 leading-none flex">
+                <div className="flex items-center">
+                    <div className="text-sm">
+                        <p className="text-gray-700 dark:text-gray-400 leading-none flex">
                             <FaUserCircle className="w-3.5 h-3.5" />
                             &nbsp;
                             {manga.author}
                         </p>
-                        <p class="text-gray-700 dark:text-gray-400">Last Updated: {manga.updated}</p>
-                        <p class="text-gray-700 dark:text-gray-400">Latest Chapter: {manga.latest_chapter}</p>
+                        <p className="text-gray-700 dark:text-gray-400">Last Updated: {manga.updated}</p>
+                        <p className="text-gray-700 dark:text-gray-400">Latest Chapter: {manga.latest_chapter}</p>
                     </div>
                 </div>
             </div>
