79ebc20a6e3a2105b240b2ff55993c6376bce2bb
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-07-24 23:21:25 -0700

    Update cats.js

--- pages/cats.js
+++ pages/cats.js
@@ -15,7 +15,7 @@ export default function Cats() {
                 <p className="prose text-gray-600 dark:text-gray-400 mb-4">
                     An image scroller that auto populates with cat images. This page leverages useSWRInfinite() to get
                     the images from <a href={"https://thecatapi.com/"} target="_blank" rel="noopener noreferrer">The Cat API.</a> If
-                    it says there are no more cat pics that means I've hit my free API limit and you'll have to come back next month.
+                    it says there are no more cat pics that means I've hit my free API limit or the API is down so you'll have to come back later.
                 </p>
                 <div className="my-2 container flex flex-wrap mx-auto">
                     <CatPics />
