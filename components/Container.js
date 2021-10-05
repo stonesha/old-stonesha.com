cf5d681f24128171750fe547fa6a3c72270fbfef
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-10-05 06:41:30 -0700

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -27,23 +27,6 @@ export default function Container(props) {
   return (
     <div className="bg-white dark:bg-black">
       <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-        />
-        <script
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-          }}
-        />
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
