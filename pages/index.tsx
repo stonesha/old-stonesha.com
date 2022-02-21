c005cba8d08d8531d2ce70e58bcebd9c92a875a9
Stone Sha <36431006+stonesha@users.noreply.github.com>
2022-02-20 20:16:04 -0800

    implemented simple fullpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
 import Container from "@/components/Container";
+import ReactFullpage from '@fullpage/react-fullpage';
 
 const IndexPage = () => (
   <Container>
-    <h1>Hello</h1>
+    <ReactFullpage
+      //fullpage options
+      licenseKey = {process.env.NEXT_PUBLIC_FULLSCREEN_LICENSE_KEY}
+      scrollingSpeed = {1000} /* Options here */
+
+      render={() => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="bg-honeydew h-screen section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+            </div>
+            <div className="bg-cerulean h-screen section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+            </div>
+            <div className="bg-champagne h-screen section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+            </div>
+            <div className="bg-salmon h-screen section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+            </div>
+            <div className="bg-saffron h-screen section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   </Container>
 )
 
